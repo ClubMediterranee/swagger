2342eaf1714e81a830c72e5adb5d3706dbe28cd3
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-05-19 13:50:42 +0200

    fix(swagger-ui): Fix problem with input parameters

--- packages/swagger-ui/src/components/SwaggerUI.js
+++ packages/swagger-ui/src/components/SwaggerUI.js
@@ -1,34 +1,98 @@
-import React, { useState } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-async function createSwaggerUI (config) {
-  const Plugins = await import(/* webpackChunkName: "plugins" */'@clubmed/plugins')
-  const { default: NativeSwaggerUI } = await import(/* webpackChunkName: "swagger-ui" */ 'swagger-ui')
-
-  const PLUGINS = { ...NativeSwaggerUI.plugins, ...Plugins }
-  const PRESETS = { ...NativeSwaggerUI.presets }
-
-  config.plugins = config.plugins.map((plugin) => PLUGINS[plugin]).filter(Boolean)
-  config.presets = config.presets.map((preset) => PRESETS[preset]).filter(Boolean)
-  console.log('config.plugins', config.plugins)
-  const ui = new NativeSwaggerUI(config)
-  // console.log(ui.getComponent('App', 'root'))
-  const Cmp = ui.getComponent('App', 'root')
-
-  return <div><Cmp/></div>
-}
-
-export default function SwaggerUI ({ config }) {
-  const [Cmp, setCmp] = useState(null)
-
-  React.useEffect(() => {
-    createSwaggerUI(config).then((Cmp) => {
-      setCmp(Cmp)
-    })
-  }, [config, setCmp])
-
-  if (!Cmp) {
-    return null
+export default class SwaggerUI extends React.Component {
+  constructor (props) {
+    super(props)
+    this.SwaggerUIComponent = null
+    this.system = null
   }
 
-  return <React.Fragment>{Cmp}</React.Fragment>
+  async componentDidMount () {
+    const { default: CoreSwaggerUI } = await import(/* webpackChunkName: "swagger-ui" */'swagger-ui-react/swagger-ui')
+    const Plugins = await import(/* webpackChunkName: "plugins" */'@clubmed/plugins')
+
+    const { layout, ...config } = this.props
+    const PLUGINS = { ...CoreSwaggerUI.plugins, ...Plugins }
+    const PRESETS = { ...CoreSwaggerUI.presets }
+
+    config.plugins = (config.plugins || []).map((plugin) => PLUGINS[plugin]).filter(Boolean)
+    config.presets = (config.presets || []).map((preset) => PRESETS[preset]).filter(Boolean)
+
+    const ui = CoreSwaggerUI({
+      ...config,
+      docExpansion: config.docExpansion || 'list',
+      requestInterceptor: this.requestInterceptor,
+      responseInterceptor: this.responseInterceptor,
+      onComplete: this.onComplete
+    })
+
+    this.system = ui
+    this.SwaggerUIComponent = ui.getComponent('App', 'root')
+
+    this.forceUpdate()
+  }
+
+  render () {
+    return this.SwaggerUIComponent ? <this.SwaggerUIComponent/> : null
+  }
+
+  componentDidUpdate (prevProps) {
+    if (this.props.url !== prevProps.url) {
+      // flush current content
+      this.system.specActions.updateSpec('')
+
+      if (this.props.url) {
+        // update the internal URL
+        this.system.specActions.updateUrl(this.props.url)
+        // trigger remote definition fetch
+        this.system.specActions.download(this.props.url)
+      }
+    }
+
+    if (this.props.spec !== prevProps.spec && this.props.spec) {
+      if (typeof this.props.spec === 'object') {
+        this.system.specActions.updateSpec(JSON.stringify(this.props.spec))
+      } else {
+        this.system.specActions.updateSpec(this.props.spec)
+      }
+    }
+  }
+
+  requestInterceptor = (req) => {
+    if (typeof this.props.requestInterceptor === 'function') {
+      return this.props.requestInterceptor(req)
+    }
+    return req
+  }
+
+  responseInterceptor = (res) => {
+    if (typeof this.props.responseInterceptor === 'function') {
+      return this.props.responseInterceptor(res)
+    }
+    return res
+  }
+
+  onComplete = () => {
+    if (typeof this.props.onComplete === 'function') {
+      return this.props.onComplete(this.system)
+    }
+  }
+}
+
+SwaggerUI.propTypes = {
+  spec: PropTypes.oneOf([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+  url: PropTypes.string,
+  requestInterceptor: PropTypes.func,
+  responseInterceptor: PropTypes.func,
+  onComplete: PropTypes.func,
+  docExpansion: PropTypes.oneOf(['list', 'full', 'none']),
+  supportedSubmitMethods: PropTypes.arrayOf(
+    PropTypes.oneOf(['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'])
+  ),
+  defaultModelExpandDepth: PropTypes.number,
+  plugins: PropTypes.arrayOf(PropTypes.object)
 }
