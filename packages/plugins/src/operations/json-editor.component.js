08621f76ffbe38c213028ca577e4ba8cba6fe848
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-10-08 14:23:03 +0200

    fix(swagger): Fix cursor issue on JsonEditor

--- packages/plugins/src/operations/json-editor.component.js
+++ packages/plugins/src/operations/json-editor.component.js
@@ -1,5 +1,5 @@
 import { getRandomComponentId } from '@clubmed/components'
-import React from 'react'
+import React, { useState } from 'react'
 import JSONInput from 'react-json-editor-ajrm/es'
 import locale from 'react-json-editor-ajrm/locale/en'
 
@@ -20,17 +20,20 @@ function parse (value) {
   return undefined
 }
 
-export default function JsonEditorComponent ({ onChange, ...props }) {
+export default function JsonEditorComponent ({ onChange, value }) {
+  const [localValue] = useState(parse(value))
+
   return <div className="mt-1 p-1 rounded-small" style={{ background: 'rgb(51, 51, 51)' }}>
     <JSONInput
       locale={locale}
       id={getRandomComponentId()}
-      {...props}
-      onChange={(evt) => onChange({ target: { value: evt.json } })}
+      onChange={(evt) => {
+        onChange({ target: { value: evt.json } })
+      }}
       className={'rounded-small'}
       colors={themes}
       style={styles}
       height='392px'
       width='100%'
-      placeholder={parse(props.value)}/></div>
+      placeholder={localValue}/></div>
 }
