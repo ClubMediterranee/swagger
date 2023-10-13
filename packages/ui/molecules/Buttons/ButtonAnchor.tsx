457fbadc62a838a898e4c52d2fcde39b3a8f69da
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-13 16:25:59 +0200

    feat(ui): improve button anchor to manage Link and external link

--- packages/ui/molecules/Buttons/ButtonAnchor.tsx
+++ packages/ui/molecules/Buttons/ButtonAnchor.tsx
@@ -1,58 +1,86 @@
-'use client';
+"use client";
 
-import { animated } from '@react-spring/web';
-import classNames from 'classnames';
-import type { AnchorHTMLAttributes, FunctionComponent } from 'react';
+import {animated} from "@react-spring/web";
+import {Link} from "react-router-dom";
+import classNames from "classnames";
+import type {AnchorHTMLAttributes, FunctionComponent} from "react";
+import React from "react";
 
-import { type CommonButtonProps } from './Button';
-import { rootClassName, variants } from './Button.helpers';
-import { themes } from './Button.themes';
-import { ButtonContent } from './ButtonContent';
+import {type CommonButtonProps} from "./Button";
+import {rootClassName, variants} from "./Button.helpers";
+import {themes} from "./Button.themes";
+import {ButtonContent} from "./ButtonContent";
 
-import { useSafeBoop } from '../../hooks/useSafeBoop';
+import {useSafeBoop} from "../../hooks/useSafeBoop";
 
-interface ButtonAnchorProps extends CommonButtonProps, AnchorHTMLAttributes<HTMLAnchorElement> {}
+interface ButtonAnchorProps extends CommonButtonProps, AnchorHTMLAttributes<HTMLAnchorElement> {
+}
+
+const AnimatedLink = animated(Link);
 
 export const ButtonAnchor: FunctionComponent<ButtonAnchorProps> = ({
-  theme = 'yellow',
-  backgroundOverride,
-  variant = 'text',
-  className,
-  label,
-  children,
-  title,
-  icon,
-  dataTestId,
-  href,
-  groupName = '',
-  onClick,
-  ...anchorAttrs
-}) => {
-  const shouldShowLabel = variant === 'text';
+                                                                     theme = "yellow",
+                                                                     backgroundOverride,
+                                                                     variant = "text",
+                                                                     className,
+                                                                     label,
+                                                                     children,
+                                                                     title,
+                                                                     icon,
+                                                                     dataTestId,
+                                                                     href,
+                                                                     groupName = "",
+                                                                     onClick,
+                                                                     ...anchorAttrs
+                                                                   }) => {
+  const shouldShowLabel = variant === "text";
   const layout = variants[variant];
   const style = themes(theme, backgroundOverride, groupName);
   const [boopStyle, boopTrigger] = useSafeBoop({
     scale: 0.98,
-    springConfig: { tension: 280, friction: 20 },
+    springConfig: {tension: 280, friction: 20}
   });
 
-  return (
-    <animated.a
+  const btn = <ButtonContent shouldShowLabel={shouldShowLabel} label={label} icon={icon}>
+    {children}
+  </ButtonContent>;
+
+  const onClickAnimate = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    onClick?.(e);
+    boopTrigger();
+  };
+
+  const classes = classNames(rootClassName, style, layout, variant, className);
+  const titleAttr = title || label;
+
+  if (href === undefined || href.includes("://")) {
+    return <animated.a
+      href={href}
       data-name="ButtonAnchor"
       data-testid={dataTestId}
-      className={classNames(rootClassName, style, layout, variant, className)}
+      className={classes}
       style={boopStyle}
-      onClick={(e) => {
-        onClick?.(e);
-        boopTrigger();
-      }}
-      title={title || label}
-      href={href}
+      onClick={onClickAnimate}
+      title={titleAttr}
+      target="_blank"
+      rel="noopener noreferrer"
+      {...anchorAttrs}>
+      {btn}
+    </animated.a>;
+  }
+
+  return (
+    <AnimatedLink
+      to={href!}
+      data-name="ButtonAnchor"
+      data-testid={dataTestId}
+      className={classes}
+      style={boopStyle}
+      onClick={onClickAnimate}
+      title={titleAttr}
       {...anchorAttrs}
     >
-      <ButtonContent shouldShowLabel={shouldShowLabel} label={label} icon={icon}>
-        {children}
-      </ButtonContent>
-    </animated.a>
+      {btn}
+    </AnimatedLink>
   );
 };
