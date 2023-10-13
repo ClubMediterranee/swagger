41851b3cc4db661283c8687ae8db8f137443572b
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-13 12:08:50 +0200

    feat(ui): add controlled value on TextField

---
+++ packages/ui/hooks/useValue.ts
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+
+export interface UseValueProps<Value = string> {
+  name: string;
+  initialValue?: Value;
+  onChange?: (name: string, value: Value) => void;
+  defaultValue?: Value | string;
+}
+
+export function useValue<Value = string>({ name, initialValue, onChange, defaultValue = "" }: UseValueProps<Value>) {
+  const [value, setLocalValue] = useState(initialValue || defaultValue);
+
+  useEffect(() => {
+    initialValue !== void 0 && setLocalValue(initialValue);
+  }, [initialValue]);
+
+  function setValue(value2: Value) {
+    setLocalValue(value2);
+    onChange && onChange(name, value2);
+  }
+
+  function resetValue() {
+    setValue(defaultValue as Value);
+  }
+
+  return {
+    value,
+    setValue,
+    resetValue
+  };
+}
