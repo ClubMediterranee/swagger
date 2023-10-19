e3ac1eb9297221a931731df15847ab4bdfa494f4
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-19 12:15:58 +0200

    feat(swagger-ui-v3): override markdown to allow css styling

---
+++ packages/swagger-ui-v3/src/plugins/form/markdown.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import cx from "classnames";
+import {Remarkable} from "remarkable";
+import {System} from "../../interfaces/System";
+
+const parser = new Remarkable("commonmark");
+parser.block.ruler.enable(["table"]);
+parser.set({linkTarget: "_blank"});
+
+export const Markdown = ({source, className = ""}: System & { source: any, className: any }) => {
+  if (typeof source !== "string") {
+    return null;
+  }
+
+  if (source) {
+    const html = parser.render(source);
+
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html.trim()
+        }}
+        className={cx(className, "renderedMarkdown")}
+      />
+    );
+  }
+  return null;
+};
+
+
+
