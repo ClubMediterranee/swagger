f14034e3a12989f99ea092fa3f2139197681e14f
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-19 12:41:21 +0200

    fix(swagger-ui-v3): fix loader when navigating in the website

--- packages/swagger-ui-v3/src/views/swagger/swagger.view.tsx
+++ packages/swagger-ui-v3/src/views/swagger/swagger.view.tsx
@@ -1,13 +1,16 @@
 import {System} from "../../interfaces/System";
 import {Loader} from "@clubmed/ui/molecules/Loader";
 import React, {useEffect} from "react";
+import {Simulate} from "react-dom/test-utils";
+import load = Simulate.load;
 
 export function SwaggerView(props: System) {
   const {getComponent, specSelectors} = props;
 
   const BaseLayout = getComponent("BaseLayout", true);
   const loadingStatus = specSelectors.loadingStatus();
-  const [loading, setLoading] = React.useState(true);
+
+  const [loading, setLoading] = React.useState(loadingStatus === "loading");
 
   useEffect(() => {
     if (loadingStatus === "loading") {
