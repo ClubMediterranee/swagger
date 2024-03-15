c89b23aaa14b8b59c6160c1cd017a5c9b00d8c97
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-03-15 15:13:22 +0100

    feat: bump repository version
    BREAKING CHANGE: new swagger doc is based on Trident UI and vite bundler

--- release.config.mjs
+++ release.config.mjs
@@ -1,11 +1,12 @@
 import { getSemanticConfig } from "@cmflow/cli";
+import omit from 'lodash/omit'
 
 process.env.PRODUCTION_BRANCH = "main";
 process.env.DEVELOP_BRANCH = "main";
 process.env.DEPLOY_ON_DOCKER = "false";
 
 export default {
-  ...getSemanticConfig(),
+  ...omit(getSemanticConfig(), ['parserOpts' | 'writerOpts']),
   npmPublish: false,
   // eslint-disable-next-line no-template-curly-in-string
   tagFormat: "v${version}"
