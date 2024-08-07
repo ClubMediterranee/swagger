40672c02952c5ac6deaaef0b790d25b87e25bf6d
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-08-07 17:39:41 +0200

    fix: replace Choices.js by react-select

--- packages/ui/molecules/Forms/Select/Select.tsx
+++ packages/ui/molecules/Forms/Select/Select.tsx
@@ -1,11 +1,46 @@
 import "./Select.css";
 
+import type { IconicNames } from "@clubmed/trident-ui/atoms/Icons";
 import { FormControl } from "@clubmed/trident-ui/molecules/Forms/FormControl";
-import omit from "lodash/omit";
+import ReactSelect from "react-select";
+import makeAnimated from "react-select/animated";
 
-import { AllSelectProps, useChoice } from "./useChoice";
+import { AllSelectProps, SelectOptionProps } from "./Select.interfaces";
+
+const animatedComponents = makeAnimated();
 
 let uuid = 0;
+
+export interface SelectProps extends Omit<React.HTMLAttributes<HTMLSelectElement>, "onChange"> {
+  /**
+   * name
+   */
+  name?: string;
+  /**
+   * label
+   */
+  label?: string;
+  /**
+   * Status of the input
+   */
+  status?: "error" | "success" | "default";
+  /**
+   * Icon name
+   */
+  icon?: IconicNames;
+  /**
+   * Error message
+   */
+  errorMessage?: string;
+  description?: string;
+  dataTestId?: string;
+  options: SelectOptionProps[];
+  placeholder?: string;
+  disableSearch?: boolean;
+  style?: any;
+  disabled?: boolean;
+  searchEnabled?: boolean;
+}
 
 export function Select(props: AllSelectProps) {
   const {
@@ -14,20 +49,39 @@ export function Select(props: AllSelectProps) {
     id = `field-date-${++uuid}`,
     label,
     status = "default",
-    icon = "",
     errorMessage,
     disabled = false,
     description = "",
-    dataTestId = "SelectField",
-    onChange: initialOnChange,
-    ...rest
+    dataTestId = "SelectField"
   } = props;
 
-  const { ref } = useChoice(props);
-
   return (
-    <FormControl dataTestId={dataTestId} dataName={"Select"} label={label} description={description}>
-      <select {...omit(rest, ["options", "defaultValue", "value"])} disabled={disabled} ref={ref} name={name} id={id} />
+    <FormControl
+      className={className}
+      dataTestId={dataTestId}
+      dataName={"Select"}
+      label={label}
+      description={description}
+      validationStatus={status}
+      errorMessage={errorMessage}
+    >
+      <ReactSelect<{ label: string; value: never | never[] }>
+        id={id}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        name={description}
+        components={animatedComponents}
+        options={props.options as any}
+        isDisabled={disabled}
+        isSearchable={true}
+        value={props.options.find((t) => t.value === props.value) as any}
+        onChange={(value) => {
+          // @ts-ignore
+          const v = (props.multiple ? value.map((v) => v.value) : (value as any).value) as any;
+
+          props.onChange && props.onChange(name, v);
+        }}
+      />
     </FormControl>
   );
 }
