335a39524a3d5193cfa3b9b0b7f7ede6960df0d8
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-09-05 08:28:23 +0200

    fix(swagger): fix styling issue

--- packages/swagger-ui-plugins/plugins/operations/request-body.component.tsx
+++ packages/swagger-ui-plugins/plugins/operations/request-body.component.tsx
@@ -4,23 +4,25 @@ import { FunctionComponent } from "react";
 import type { System } from "../../interfaces/System";
 
 export function wrapRequestBody(Base: FunctionComponent<Record<string, unknown>>) {
-  return (props: System & { requestBody?: any; contentType: any; specPath: any }) => {
+  return (props: System & { requestBody?: any; contentType: any; specPath: any; isExecute: boolean }) => {
     const { getComponent, requestBody, specPath } = props;
     const ModelExample = getComponent("modelExample");
     const requestBodyContent = requestBody?.get("content") ?? OrderedMap();
     const contentType = props.contentType || requestBodyContent.keySeq().first() || "";
     const mediaTypeValue = requestBodyContent.get(contentType) ?? OrderedMap();
 
-    return (
+    return props.isExecute ? (
       <ModelExample
         {...props}
         expandDepth={1}
-        isExecute={true}
+        isExecute={props.isExecute}
         schema={mediaTypeValue.get("schema")}
         specPath={specPath.push("content", contentType)}
         example={<Base {...props} />}
         includeWriteOnly={true}
       />
+    ) : (
+      <Base {...props} />
     );
   };
 }
