c28a6f4585d14d6e206885970a43bf78c0ac5dbd
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-10-14 09:42:39 +0200

    fix: update baseName logic to handle '/doc' path in window origin

--- packages/swagger-ui-v3/src/main.tsx
+++ packages/swagger-ui-v3/src/main.tsx
@@ -15,7 +15,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-const baseName = (window as any).basename || "/";
+const baseName = window.origin.includes("/doc") ? "/doc" : (window as any).basename || "/";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
